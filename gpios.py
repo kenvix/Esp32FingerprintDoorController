015a8d212063a93d5d2fd1da8ff07214d998c2fb
Kenvix Zure <kenvixzure@live.com>
2022-02-12 12:17:40 +0800

    Motor PWM

--- gpios.py
+++ gpios.py
@@ -16,8 +16,10 @@ pinBootButton: Pin
 pinBootButtonIrqHandler = lambda _: log.debug("Button Boot Pressed")
 fingerSession: Serial = None
 fingerWakPin: Pin = None
+isDoorOperating = False
 pinFingerWakIrqHandler = lambda _: log.debug("Finger wak pin pressed")
 pinMotor: Pin
+motorPwmTimer = Timer(1)
 
 def loadPin():
     global pinStatus, pinBootButton, pinLight, pinBootButtonIrqHandler, fingerSession, fingerWakPin, timerStatus, pinMotor
@@ -69,14 +71,53 @@ def loadFinger():
                 handler=pinFingerWakIrqHandler
             )
 
+def _doorTimerDeinit(timer: Timer, pwm: PWM):
+    global isDoorOperating
+    pwm.deinit()
+    timer.deinit()
+    isDoorOperating = False
+
 
 def doorOpen():
-    pwm = PWM()
+    global isDoorOperating
+    if isDoorOperating:
+        return
+    isDoorOperating = True
+    pwm = PWM(pinMotor, freq=gpioconfig.MOTOR_PWM_OPEN_FREQ, duty=gpioconfig.MOTOR_PWM_OPEN_DUTY)
+    motorPwmTimer.init(period=gpioconfig.MOTOR_ROLLTATE_DELAY, mode=Timer.ONE_SHOT, callback=lambda x: _doorTimerDeinit(x, pwm))
     pass
 
 
 def doorClose():
+    global isDoorOperating
+    if isDoorOperating:
+        return
+    isDoorOperating = True
+    pwm = PWM(pinMotor, freq=gpioconfig.MOTOR_PWM_CLOSE_FREQ, duty=gpioconfig.MOTOR_PWM_CLOSE_DUTY)
+    motorPwmTimer.init(period=gpioconfig.MOTOR_ROLLTATE_DELAY, mode=Timer.ONE_SHOT, callback=lambda x: _doorTimerDeinit(x, pwm))
     pass
+
+
+def doorOpenAndClose():
+    global isDoorOperating
+    if isDoorOperating:
+        return
+    isDoorOperating = True
+    pwm = PWM(pinMotor, freq=gpioconfig.MOTOR_PWM_OPEN_FREQ, duty=gpioconfig.MOTOR_PWM_OPEN_DUTY)
+    motorPwmTimer.init(period=gpioconfig.MOTOR_ROLLTATE_DELAY, mode=Timer.ONE_SHOT, callback=lambda x: _doorOpenAndClose2(x, pwm))
+
+
+def _doorOpenAndClose2(timer: Timer, pwm: PWM):
+    global isDoorOperating
+    pwm.deinit()
+    timer.deinit()
+    motorPwmTimer.init(period=gpioconfig.DOOR_AUTOCLOSE_DELAY, mode=Timer.ONE_SHOT, callback=lambda x: _doorOpenAndClose3(x, pwm))
+
+
+def _doorOpenAndClose3(timer: Timer, pwm: PWM):
+    global isDoorOperating
+    pwm = PWM(pinMotor, freq=gpioconfig.MOTOR_PWM_CLOSE_FREQ, duty=gpioconfig.MOTOR_PWM_CLOSE_DUTY)
+    motorPwmTimer.init(period=gpioconfig.MOTOR_ROLLTATE_DELAY, mode=Timer.ONE_SHOT, callback=lambda x: _doorTimerDeinit(x, pwm))
 
 
 def startDoorController():
