e274f43b1c96195f3ec6039e42cc5f1e674dfe8c
Kenvix Zure <kenvixzure@live.com>
2022-02-12 11:54:21 +0800

    Do not use PWM

--- gpios.py
+++ gpios.py
@@ -1,4 +1,3 @@
-import re
 from machine import Timer
 from machine import Pin
 import machine
@@ -11,41 +10,66 @@ if gpioconfig.FINGER_ENABLE:
     import as608
 
 tmd = None
-pinStatus: Pin 
+pinStatus: Pin
 timerStatus: Timer
 pinBootButton: Pin
-pinBootButtonIrqHandler = lambda _: log.debug("Button Boot Pressed")
+
+
+def pinBootButtonIrqHandler(_): return log.debug("Button Boot Pressed")
+
+
 fingerSession: Serial = None
 fingerWakPin: Pin = None
-pinFingerWakIrqHandler = lambda _: log.debug("Finger wak pin pressed")
+
+
+def pinFingerWakIrqHandler(_): return log.debug("Finger wak pin pressed")
+
 
 def loadPin():
     global pinStatus, pinBootButton, pinLight, pinBootButtonIrqHandler, fingerSession, fingerWakPin, timerStatus
     pinStatus: Pin = machine.Pin(gpioconfig.LED_STATUS_PIN, Pin.OUT)
     pinBootButton: Pin = machine.Pin(0, machine.Pin.IN, machine.Pin.PULL_UP)
     pinBootButton.irq(
-        trigger=Pin.IRQ_FALLING, 
+        trigger=Pin.IRQ_FALLING,
         handler=pinBootButtonIrqHandler
     )
+    # timerStatus = None
     timerStatus = Timer(-1)
 
-def blinkStatusLED():
-    timerStatus.init(period=350, mode=Timer.PERIODIC, callback=lambda x: pinStatus.value(not pinStatus.value()))
+# def blinkStatusLED(freq = 3, duty = 20):
+#     global timerStatus
+#     if timerStatus is not None:
+#         timerStatus.deinit()
+
+#     timerStatus = PWM(pinStatus, freq=freq, duty=duty)
+#     return timerStatus
+
+
+def blinkStatusLED(period=350):
+    global timerStatus
+    timerStatus.init(period=period, mode=Timer.PERIODIC, callback=lambda x: pinStatus.value(not pinStatus.value()))
+    return timerStatus
+
 
 def cancelBlinkStatusLED():
+    global timerStatus
     timerStatus.deinit()
+
 
 def reboot():
     pinReset: Pin = machine.Pin(gpioconfig.REBOOT_PIN, Pin.OUT)
     pinReset.value(1)
 
+
 def loadFinger():
+    global fingerSession, fingerWakPin
     if gpioconfig.FINGER_ENABLE:
-        fingerSession = as608.connect_serial_session(gpioconfig.FINGER_UART_PORT)
+        fingerSession = as608.connect_serial_session(
+            gpioconfig.FINGER_UART_PORT)
         fingerWakPin = machine.Pin(gpioconfig.FINGER_WAK_PIN, Pin.IN)
         if gpioconfig.FINGER_WAK_IRQ_ENABLE:
             fingerWakPin.irq(
-                trigger=Pin.IRQ_RISING, 
+                trigger=Pin.IRQ_RISING,
                 handler=pinFingerWakIrqHandler
             )
 
@@ -53,8 +77,10 @@ def loadFinger():
 def doorOpen():
     pass
 
+
 def doorClose():
     pass
+
 
 def startDoorController():
     log.info("Starting door controller")
